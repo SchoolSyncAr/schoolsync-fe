7d4c8e0b2d647f2487c789ebca55a96371dc5d8f
JuanTexeira <juansetex@gmail.com>
2024-04-24 18:41:32 -0300

    Se realiza refactor en router y en notificationService. A su vez se implementa contra el backend el contador de las notificaciones del navbar

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,31 @@
-import { BrowserRouter as Router, Route, Routes, Navigate, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import ParentDashboard from './Components/ParentDashboard'
 import NotificationsDashboard from './Components/NotificationsDashboard'
 import Children from './Components/Children'
 import AdminDashboard from './Components/AdminDashboard'
-
+import { LayoutWrap } from './Components/LayoutWrap'
 
 export const MyRoutes = () => (
-  <Router>
-    <Routes>
-      <Route path="/login" element={<ParentDashboard />} />
-      <Route path="/parentDashboard" element={<ParentDashboard />} />
-      <Route path="/notificationsDashboard" element={<NotificationsDashboard />} />
-      <Route path='/children' element={<Children />} />
-      <Route path='/adminDashboard' element={<AdminDashboard />} />
-      <Route path='*' element={<Navigate to='/login' />} /> 
-    </Routes>
+  <Routes>
+    {/* Aca iría login */}
+    <Route path="" element={<ParentDashboard />} />
+    <Route path="/" element={<LayoutWrap />}>
+      {/* <Route path="/login" element={<ParentDashboard />} /> */}
+      <Route path="parentDashboard" element={<ParentDashboard />} />
+      <Route path="notificationsDashboard" element={<NotificationsDashboard />} />
+      <Route path="children" element={<Children />} />
+      <Route path="adminDashboard" element={<AdminDashboard />} />
+    </Route>
 
-    <Link to="/login">Iniciar sesión</Link>
-  </Router>
+    <Route path="*" element={<Navigate to="" />} />
+    {/* <Link to="/login">Iniciar sesión</Link> */}
+  </Routes>
 )
+
+export const SchoolSyncRouter = () => {
+  return (
+    <Router>
+      <MyRoutes />
+    </Router>
+  )
+}
