2f73f2bbcbc8e93ffa760e0506384b034926cc57
Calipas <cmpastorino@estudiantes.unsam.edu.ar>
2024-04-26 21:10:02 -0300

    createNewNotification service conexion problem with backend, solved

--- src/Services/NotificationService.tsx
+++ src/Services/NotificationService.tsx
@@ -10,13 +10,12 @@ class NotificationService {
     )
   }
 
-  createNotification = async (notification: { title: string; content: string }) => {
-    const response = await fetch(`${REST_SERVER_URL}/createNotifications`, {
-      method: 'POST',
-      body: JSON.stringify(notification),
-    })
-    const result = await response.json()
-    return result
+  createNotification = async (notification: {
+      title: string
+      content: string
+    }) => {
+    const response = await axios.post(`${REST_SERVER_URL}/createNotifications`, notification)
+    return response.data
   }
 
   getNotificationsCount = async () => {
