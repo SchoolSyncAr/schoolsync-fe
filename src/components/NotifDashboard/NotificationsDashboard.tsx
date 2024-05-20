0a486fa23e283a06ad330d5caa9e3ce55dd1959a
francopolesel <francopolesel99@gmail.com>
2024-05-20 16:32:21 -0300

    Filter for notifications is added

--- src/components/NotifDashboard/NotificationsDashboard.tsx
+++ src/components/NotifDashboard/NotificationsDashboard.tsx
@@ -1,34 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import Button from 'components/Button'
-import { useNavigate } from 'react-router-dom'
 import { NotifCard } from 'components/NotifCard/NotifCard'
-import { useState } from 'react'
 import { useOnInit } from 'utils/useOnInit'
 import './NotificationDashboard.css'
 import { NotifProps } from 'interfaces/Notification'
-import notificationService  from 'services/NotificationService'
+import notificationService from 'services/NotificationService'
 import SearchBar from 'components/Searchbar/Searchbar'
 
 function NotificationsDashboard() {
   const [notifications, setNotifications] = useState<NotifProps[]>([])
+  const [params, setParams] = useSearchParams()
   const [errorMessage, setErrorMessage] = useState('')
-  const [searchField, setSearchField] = useState('')
+  const [filter, setFilter] = useState({
+    searchField: params.get('searchField') || '',
+    orderParam: params.get('sortData') || '',
+    sortDirection: params.get('sortDirection') || '',
+  })
 
   useOnInit(async () => {
     try {
-      const notifs = await notificationService.getAllGeneralNotifications()
-      console.log('desde useOnInit')
-      console.log(notifs)
+      const notifs = await notificationService.getAllGeneralNotifications(filter)
       setNotifications(notifs)
-      console.log('desde useOnInit longitud segundo  ' + notifications.length)
     } catch {
       setErrorMessage('No se pudo obtener info notifications')
     }
   })
 
+  useEffect(() => {
+    const newParams = new URLSearchParams()
+    newParams.append('searchField', filter.searchField)
+    newParams.append('orderParam', filter.orderParam)
+    newParams.append('sortDirection', filter.sortDirection)
+    setParams(newParams)
+  }, [filter, setParams])
+
   const handleSearchInit = () => {
     useOnInit(async () => {
       try {
-        const notifs = await notificationService.getAllGeneralNotifications()
+        const notifs = await notificationService.getAllGeneralNotifications(filter)
         setNotifications(notifs)
       } catch {
         setErrorMessage('No se pudo obtener info notifications')
@@ -37,18 +47,37 @@ function NotificationsDashboard() {
   }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchField(event.target.value)
+    const { name, value, type, checked } = event.target
+    const param = type === 'checkbox' ? checked : value
+    setFilter((prevfilter) => ({
+      ...prevfilter,
+      [name]: param,
+    }))
+  }
+
+  const handleFilterChange = (filterName: string, filterValue: string) => {
+    setFilter((prevfilter) => ({
+      ...prevfilter,
+      [filterName]: filterValue,
+    }))
   }
 
   const notifList = () => {
-    return notifications.map((data) => <NotifCard id={data.id} title={data.title} content={data.content} />)
+    return notifications.map((data) => (
+      <NotifCard key={data.id} id={data.id} title={data.title} content={data.content} />
+    ))
   }
 
   const navigate = useNavigate()
 
   return (
     <>
-      <SearchBar handleSearchInit={handleSearchInit} handleChange={handleChange} searchField={searchField} />
+      <SearchBar
+        handleSearchInit={handleSearchInit}
+        handleChange={handleChange}
+        filter={filter}
+        handleFilterChange={handleFilterChange} // Pasa la nueva prop
+      />
       <div className="notif-grid">{notifList()}</div>
       <br></br>
       <br></br>
