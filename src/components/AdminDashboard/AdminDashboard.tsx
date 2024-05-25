bfd1600e5f36954a07eb71a0e27a681008beccd1
lopez-sol <lopez.msol.07@gmail.com>
2024-05-25 12:04:41 -0300

    refactor de estilo en vista para crear notificaciones

--- src/components/AdminDashboard/AdminDashboard.tsx
+++ src/components/AdminDashboard/AdminDashboard.tsx
@@ -3,12 +3,24 @@ import  notificationService  from 'services/NotificationService'
 import Button from '../Button'
 import './AdminDashboard.scss'
 import { useNavigate } from 'react-router-dom'
+import { FormControl, FormHelperText, MenuItem, Select, SelectChangeEvent } from '@mui/material'
 
 function AdminDashboard() {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [weight, setWeight] = useState('')
+  const [recipients, setRecipients] = useState<string[]>([])
+
 
   const navigate = useNavigate()
+
+  function getRecipients() {
+    return ['Todos','1er Grado','2do Grado','3er Grado']
+  }
+
+  function getWeights() {
+    return ['Alto','Medio','Bajo']
+  }
 
   function titleInputHandler(event: { target: { value: SetStateAction<string> } }) {
     setTitle(event.target.value)
@@ -16,6 +28,19 @@ function AdminDashboard() {
 
   function contentInputHandler(event: { target: { value: SetStateAction<string> } }) {
     setContent(event.target.value)
+  }
+
+  function weightChangeHandler(event: { target: { value: SetStateAction<string> } }) {
+    setWeight(event.target.value)
+  }
+
+  function recipientChangeHandler (event: SelectChangeEvent<string[]>) {
+    const {
+      target: { value },
+    } = event
+    setRecipients(
+      typeof value === 'string' ? value.split(',') : value,
+    )
   }
 
   function createNewNotificationEventHandler(event: { preventDefault: () => void }) {
@@ -43,49 +68,92 @@ function AdminDashboard() {
         <section className='new-notif_header'>
           <h3>Crear Notificacion</h3>
         </section>
-        <form onSubmit={createNewNotificationEventHandler}>
-          <div className="new-notif_container">
-            <label className="field__label text text--light">Titulo</label>
-            <input
-              onChange={titleInputHandler}
-              value={title}
-              className="field field--rounded animated shadow"
-              autoFocus={true}
-              required
-            />
-          </div>
-          <div className="field__container">
-            <label className="field__label text text--light">Contenido</label>
-            <input
-              onChange={contentInputHandler}
-              value={content}
-              className="field field--rounded animated shadow"
-              autoFocus={true}
-              required
-            />
-          </div>
-          {/* <div className="container">
-            <div className="row mt-5">
-              <ul className="list-group shadow"></ul>
-              <input type="text" onChange={titleInputHandler} placeholder="Titulo de la Notificacion" value={title} />
-              <textarea
-                onChange={contentInputHandler}
-                name="notificationContent"
-                value={content}
-                placeholder="Contenido"
-                cols={30}
-                rows={10}
-              ></textarea>
-              <button className="button button1" type="submit">
-                Guardar
-              </button>
-              <div className="buttonsToRightEnd">
-                <Button className='forAllButtons buttonReturn' height={60} actionOnClick={() => navigate('/parentDashboard')}>Volver</Button>
-              </div>
+
+        <form onSubmit={createNewNotificationEventHandler} className='new-notif_form'>
+          <div onSubmit={createNewNotificationEventHandler} className='new-notif_body'>
+            <div className="field__container">
+              <input
+                id="newNotifTitle"
+                className="field field--rounded animated shadow"
+                onChange={titleInputHandler}
+                value={title}
+                autoFocus={true}
+                data-testid="login-username"
+                required
+              />
+              <label className="field__label text text--light" htmlFor="newNotifTitle">
+                Titulo
+              </label>
             </div>
-          </div> */}
+            <div className="field__container">
+              <textarea
+                id="newNotifContent"
+                onChange={contentInputHandler}
+                value={content}
+                className="field field--textarea field--rounded animated shadow"
+                required
+              />
+              <label className="field__label--textarea text text--light" htmlFor="newNotifContent">
+                Contenido
+              </label>
+            </div>
+          </div>
+          <div className='new-notif_settings'>
+            <div className='new-notif_settings-section'>
+              <label className="text text--white text--md text--strong">
+                Peso
+              </label>
+              <FormControl className="field field--select field--rounded animated shadow">
+                <Select
+                  value={weight}
+                  onChange={weightChangeHandler}
+                  className="field field--select field--rounded shadow"
+                >
+                  {getWeights().map((weight) => (
+                    <MenuItem
+                      key={weight}
+                      value={weight}
+                    >
+                      {weight}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+            <div className='new-notif_settings-section'>
+              <label className="text text--white text--md text--strong">
+                A quien le llega
+              </label>
+              <FormControl className="field field--select field--rounded animated shadow">
+                <Select
+                  multiple
+                  value={recipients}
+                  onChange={recipientChangeHandler}
+                  className="field field--select field--rounded shadow"
+                >
+                  {getRecipients().map((name) => (
+                    <MenuItem
+                      key={name}
+                      value={name}
+                    >
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <FormHelperText sx={{color: 'var(--color-white)', fontFamily: 'var(--typeface-main)'}}>(Seleccione 1 o más)</FormHelperText>
+              </FormControl>
+            </div>
+          </div>
         </form>
-        <section className='go-back'>
+        <button
+          className="button button--primary button--tall button--rounded text--md text--spaced text--upper animated shadow--box"
+          // disabled={!isDirty || !isValid || isSubmitting}
+          type="submit"
+          data-testid="new-notif-submit"
+        >
+          Crear
+        </button>
+        <section className='control-buttons'>
           <Button className='forAllButtons buttonReturn' height={60} actionOnClick={() => navigate('/parentDashboard')}>Volver</Button>
         </section>
       </article>
