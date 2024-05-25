1293015121d0b442367cef2198aadc13752af4bf
Calipas <cmpastorino@estudiantes.unsam.edu.ar>
2024-05-25 10:07:33 -0300

    data separada es mas prolijo

--- src/services/prueba.tsx
+++ src/services/prueba.tsx
@@ -11,7 +11,13 @@ export const deleteNotificationById2 = async (notificationId: number, token: str
     Authorization: `Bearer ${token}`
   }
 
-  const notificationJson = await axios.delete(`http://localhost:8080/notification/deleteNotification/${notificationId}?searchField=&orderParam=date&sortDirection=asc`, { headers })
+  const data = {
+    searchField: '',
+    orderParam: 'date',
+    sortDirection: 'asc'
+  }
+
+  const notificationJson = await axios.delete(`http://localhost:8080/notification/deleteNotification/${notificationId}`, { headers, data })
   return notificationJson.data.map((notificationJson: any)=>{Notification.fromJson(notificationJson)})
 }
 
@@ -24,13 +30,27 @@ export const deleteNotificationById2 = async (notificationId: number, token: str
 
 
 
-//asi estaba antes de empezar a cambierlo 
+//asi estaba antes de empezar a cambierlo
 // import axios from "axios"
 // import { REACT_APP_REST_SERVER_URL } from "constants/constants"
 
 
-
+/*
 // export const deleteNotificationById2 = async (notificationId: number) => {
-//   const notificationJson = await axios.delete(`${REACT_APP_REST_SERVER_URL}/notification/deleteNotification/${notificationId}?searchField=&orderParam=date&sortDirection=asc`)
+//  const notificationJson = await axios.delete(`${REACT_APP_REST_SERVER_URL}/notification///deleteNotification/${notificationId}?searchField=&orderParam=date&sortDirection=asc`)
 //   return notificationJson.data.map((notificationJson: any)=>{Notification.fromJson(notificationJson)})
 // }
+
+
+//asi cuando estaba todo junto
+// export const deleteNotificationById2 = async (notificationId: number, token: string) => {
+//   console.log("deletttttttttttt")
+//   console.log(notificationId)
+//   console.log(token)
+//   const headers = {
+//     Authorization: `Bearer ${token}`
+//   }
+
+//   const notificationJson = await axios.delete(`http://localhost:8080/notification/deleteNotification/${notificationId}?searchField=&orderParam=date&sortDirection=asc`, { headers })
+//   return notificationJson.data.map((notificationJson: any)=>{Notification.fromJson(notificationJson)})
+// }*/
