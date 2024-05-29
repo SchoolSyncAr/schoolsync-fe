98a339f9d4860e3ac9fc3e70bfe24fc2671522c8
madescoces <madescoces@gmail.com>
2024-05-29 07:04:19 -0300

    Se agrega en el dashboard una linea para mostrar el user id provisoria, se crean los metodos para guardar en session storage el id de usuario y el getter del mismo en el service

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,42 +1,48 @@
 import { LoginArgs } from 'models/interfaces/types'
 import api from 'api/axios.tsx'
 
-
 const AuthService = () => {
-  const login = async (credentials: LoginArgs ) => {
+  const login = async (credentials: LoginArgs) => {
     try {
       const response = await api.post('/api/auth', credentials)
       const token = response.data.accessToken
       const role = response.data.role
+      const userId = response.data.userId
 
-      sessionStorage.setItem("token", token)
-      sessionStorage.setItem("role", role)
+      sessionStorage.setItem('token', token)
+      sessionStorage.setItem('role', role)
+      sessionStorage.setItem('userId', userId)
 
       return token
-      
-
     } catch (error) {
       throw new Error('Error en la autenticación. Por favor, verifica tus credenciales.')
     }
   }
 
   const getUserToken = () => {
-    return sessionStorage.getItem("token")
+    return sessionStorage.getItem('token')
   }
 
   const getUserRole = () => {
-    return sessionStorage.getItem("role")
+    return sessionStorage.getItem('role')
+  }
+
+  const getUserId = () => {
+    return sessionStorage.getItem('userId')
   }
 
   const clearUser = () => {
-    console.log("session clear")
-    sessionStorage.removeItem("auth")
+    console.log('session clear')
+    sessionStorage.removeItem('auth')
   }
 
   return {
-    login, getUserToken, getUserRole, clearUser
+    login,
+    getUserToken,
+    getUserRole,
+    clearUser,
+    getUserId,
   }
-
 }
 
 export const authService = AuthService()
