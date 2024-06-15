7f01d18bc341c0de8cf9d3636ec3ba56687f980e
lopez-sol <lopez.msol.07@gmail.com>
2024-06-15 16:44:39 -0300

    Comienzos de refactor...

--- src/services/ParentService.tsx
+++ src/services/ParentService.tsx
@@ -1,8 +1,9 @@
 import api from "api/axios"
 import { REACT_APP_REST_SERVER_URL } from "constants/constants"
-import { ParentProps } from "models/interfaces/Parent"
+import { ParentProps, StudentProps } from "models/interfaces/User"
 import { Parent } from "models/Parent"
 import { Student } from "models/Student"
+import { authService } from "./AuthService"
 
 // export const getAllChildrenForAParent = async (parentId: number) => {
 //   console.log("parent service parent id is: " + parentId)
@@ -10,16 +11,14 @@ import { Student } from "models/Student"
 // }
 
 class ParentService {
-  getAll = async () => {
+  getAll = async (): Promise<Parent[]>  => {
     const parents = await api.get(`${REACT_APP_REST_SERVER_URL}/api/parent/all`)
     return parents.data.map((parent: ParentProps) => Parent.fromJson(parent))
   }
 }
 export const parentService = new ParentService()
 
-export const getMyChildren = async () => {     //eventualmente hay que agregar (parentId: number)
-  console.log("estoy en getMyChildren del Student servicce")
-  const allMyChildrenJson = await api.get(`api/parent/myChildren/7`)
-  console.log("all my children " + allMyChildrenJson)
-  return allMyChildrenJson.data.map((allStudentsJson: { id: number; firstName: string; lastName: string; absences: number; notifications: any })=>Student.fromJson(allStudentsJson))
+export const getMyChildren = async (): Promise<Student[]> => {
+  const students = await api.get(`api/parent/childs/${authService.getUserId()}`)
+  return students.data.map((student: StudentProps) => new Student(student))
 }
